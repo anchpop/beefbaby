5c9be88e598ab94ef9653137377bdb381bb9d853
Andre Popovitch <andre@popovit.ch>
2024-04-24 18:52:17 -0500

    add some pics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+let BEEF_BABIES = [
+  {
+    name: "Andre",
+    bio: "Born in the beef, moulded by it... I didn't see the milk until I was already a man.",
+    image: "/beefs/andre.jpg"
+  },
+  {
+    name: "Quincy",
+    bio: "Coder and mathematician extraordinaire",
+    image: "/beefs/quincy.jpg"
+  },
+]
+
 export default function Home() {
   return (
     <main className={styles.main}>
@@ -11,17 +24,10 @@ export default function Home() {
       </div>
       <div className={styles.content}>
         <div className={styles.section}>
-          <h2 className={styles.sectionTitle}>Beef Baby Adventures</h2>
+          <h2 className={styles.sectionTitle}>Beef TV</h2>
           <div className={styles.sectionContent}>
-            {/* Add your blog posts here */}
-            <div className={styles.blogPost}>
-              <h3 className={styles.blogPostTitle}>Adventure Title 1</h3>
-              <p className={styles.blogPostDate}>Date of Adventure</p>
-              <p className={styles.blogPostContent}>
-                Write about your adventure here...
-              </p>
-            </div>
-            {/* Repeat for each blog post */}
+            <p>Watch movies and shows: <a href="https://watch.beef.baby">watch.beef.baby</a></p>
+            <p>Request movies and shows: <a href="https://request.beef.baby">request.beef.baby</a></p>
           </div>
         </div>
         <div className={styles.sectionImage}>
@@ -41,7 +47,7 @@ export default function Home() {
           />
         </div>
         <div className={styles.section}>
-          <h2 className={styles.sectionTitle}>Meet the Beef Babies</h2>
+          <h2 className={styles.sectionTitle}>Beef of the Day</h2>
           <div className={styles.sectionContent}>
             {/* Add your Beef Babies here */}
             <div className={styles.beefBabiesGrid}>
@@ -65,10 +71,15 @@ export default function Home() {
           <div className={styles.sectionContent}>
             {/* Add your blog posts here */}
             <div className={styles.blogPost}>
-              <h3 className={styles.blogPostTitle}>Adventure Title 1</h3>
-              <p className={styles.blogPostDate}>Date of Adventure</p>
+              <h3 className={styles.blogPostTitle}>Trip to Marquette</h3>
+              <p className={styles.blogPostDate}>April 20, 2024</p>
               <p className={styles.blogPostContent}>
-                Write about your adventure here...
+                <Image
+                  src="/adventure/marquette.jpg"
+                  alt="Image"
+                  width={600}
+                  height={600}
+                />
               </p>
             </div>
             {/* Repeat for each blog post */}
